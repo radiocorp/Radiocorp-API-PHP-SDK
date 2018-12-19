7e67e0f59a04149a06759b9f3f69178ad3886254
RadioCorp Dev <beheer@radiocorp.nl>
2018-12-19 15:33:36 +0100

    Minor update

--- git_push.sh
+++ git_push.sh
@@ -8,7 +8,7 @@ git_repo_id=$2
 release_note=$3
 
 if [ "$git_user_id" = "" ]; then
-    git_user_id="radiocorp"
+    git_user_id="radiocorp-dev"
     echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
 fi
 
